439567b54f8a4a01bf71dd4901e72bdf8b87687c
Divyansh Kachangal <dkachangal@gmail.com>
2026-03-31 01:42:20 +0530

    first commit from terminal

--- L-openCV/test1.py
+++ L-openCV/test1.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-
+i = 0
 while True:
     works, frame = cap.read()
     cv2.imshow('frame', frame)
+    print(works , i)
+    i+=1
 
     if (cv2.waitKey(1) == ord('q')):
         break
