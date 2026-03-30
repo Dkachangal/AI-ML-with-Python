4584ea4d14ad064b830fc0e45049001f1cccafaf
Divyansh Kachangal <dkachangal@gmail.com>
2026-03-31 01:51:57 +0530

    this is second, but from another folder

--- L-openCV/test1.py
+++ L-openCV/test1.py
@@ -5,9 +5,17 @@ cap = cv2.VideoCapture(0)
 i = 0
 while True:
     works, frame = cap.read()
-    cv2.imshow('frame', frame)
+    #COLORFUL FRAME 
+    # cv2.imshow('frame', frame)
     print(works , i)
     i+=1
+
+    bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow("New Image", bw)
+
+    # red = cv2.inRange(frame, (0, 0, 100), (100, 100, 255))
+    # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # cv2.imshow('RedFrame', rgb)
 
     if (cv2.waitKey(1) == ord('q')):
         break
